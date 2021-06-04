7dc26d000829236764eda149c1c3c55e0044e28d
josejesuscg <josejesuscg08@gmail.com>
2021-06-04 12:49:16 -0400

    por finalizar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from 'react';
 import Formulario from './components/Formulario';
+import Cita from './components/Cita';
 
 function App() {
 
@@ -13,6 +14,12 @@ function App() {
       cita
     ]);
   }
+
+  // Funcion para eliminar citas 
+  const eliminarCita = id => {
+    const nuevasCitas = citas.filter(cita => cita.id !== id)
+    guardarCitas(nuevasCitas);
+  }
   return (
     <Fragment>
       <h1>Administrador de Pacientes </h1>
@@ -25,7 +32,14 @@ function App() {
               />
             </div>
             <div className="one-half column">
-              2
+              <h2>Administra tus citas</h2>
+              {citas.map(cita => (
+                <Cita 
+                  key={cita.id}
+                  cita={cita}
+                  eliminarCita={eliminarCita}
+                />
+              ))}
             </div>
           </div>
         </div>
